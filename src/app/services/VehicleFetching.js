697afae6c325aaacd0786a0b677dce7c974a6b21
francopolesel <francopolesel99@gmail.com>
2024-08-30 16:04:58 -0300

    Implemented error handling for any data fetching issues

--- src/app/services/VehicleFetching.js
+++ src/app/services/VehicleFetching.js
@@ -1,15 +1,35 @@
 export const fetchVehicleTypes = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/vehicles/GetMakesForVehicleType/car?format=json`,
-  );
-  const data = await response.json();
-  return data.Results;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/vehicles/GetMakesForVehicleType/car?format=json`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.Results || [];
+  } catch (error) {
+    console.error("Error fetching vehicle types:", error);
+    return [];
+  }
 };
 
 export const fetchVehicleModelsByMakeAndYear = async (makeId, year) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`,
-  );
-  const data = await response.json();
-  return data.Results || [];
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.Results || [];
+  } catch (error) {
+    console.error("Error fetching vehicle models:", error);
+    return [];
+  }
 };
